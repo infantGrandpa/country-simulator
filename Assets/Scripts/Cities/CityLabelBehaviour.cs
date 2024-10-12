3765f3d4dc78997aafa56944ff8da774d584f522
Brett <babraham341@gmail.com>
2024-10-12 18:35:57 -0400

    Position city label on city

--- Assets/Scripts/Cities/CityLabelBehaviour.cs
+++ Assets/Scripts/Cities/CityLabelBehaviour.cs
@@ -1,13 +1,34 @@
+using Abraham.CountrySimulator.GUI;
+using Abraham.GalacticConquest.GUI;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-namespace CountrySimulator.Cities
+namespace Abraham.CountrySimulator.Cities
 {
     public class CityLabelBehaviour : MonoBehaviour
     {
         [SerializeField] TMP_Text cityNameLabel;
+        [SerializeField] Vector2 labelOffset;
 
-        public void SetCityName(string cityName)
+        RectTransform _rectTransform;
+
+        public void InitializeLabel(string cityName, Vector3 worldPosition)
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            
+            SetCityName(cityName);
+            SetLabelPosition(worldPosition);
+        }
+        void SetLabelPosition(Vector3 worldPosition)
+        {
+            Vector2 canvasPosition = GUIManager.Instance.mainCanvas.WorldToCanvasPosition(worldPosition, GUIManager.Instance.mainCamera);
+            Vector2 finalPosition = canvasPosition + labelOffset;
+
+            _rectTransform.anchoredPosition = finalPosition;
+        }
+
+        void SetCityName(string cityName)
         {
             cityNameLabel.text = cityName;
         }
